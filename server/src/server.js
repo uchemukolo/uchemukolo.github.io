ab666b284943d8c21d05e64c97e2aa7b89918243
uche mukolo <uchek7@gmail.com>
2017-08-30 16:32:11 +0100

    server setup

---
+++ server/src/server.js
@@ -0,0 +1,29 @@
+// Setup ======
+const express = require('express');
+const morgan = require('morgan');             // log requests to the console (express4)
+const bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+const methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+// init app
+const app = express();
+
+app.use(express.static(__dirname + '../../template/views'));    // set the static files location
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// APIs
+// app.get('/api/users/index', (req, res) =>{
+//     res.json(index);
+// });
+
+
+app.get("/", (req, res) =>{
+    res.sendFile('../../template/views/index.html');
+});
+
+app.listen(3030, () =>{
+    console.log('Express is listening on port 3030... ');
+});
