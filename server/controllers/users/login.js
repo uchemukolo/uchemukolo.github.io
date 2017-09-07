97ec83347ec5dfe68600f6e6af520b9bade18ddc
uche mukolo <uchek7@gmail.com>
2017-09-07 17:30:28 +0100

    create-login-api

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -1,125 +1,16 @@
-// const login = (req, res, next) => {
-//   console.log(req.body);
-//   next();
-
-const Userdata = require('../models').User;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  module.exports = {
-  login(req, res) {
-    return User
+import { User } from '../../models/index';
+
+const login = (req, res) =>{
+  console.log(req.body);
+  console.log("ladi is a senior dev");
+   return User
       .findOne({
-        username: req.body.username, 
-      })
+      	where: {username: req.body.username,
+              password: req.body.password}
+})
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
-  },
-}; 
-}
+  }
 
 module.exports = login;
+
